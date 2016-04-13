44714270f583cdbaf97809fa3dc452964488282b
thadino <pwntfire@hotmail.com>
2016-04-13 15:41:03 +0200

    ooo

--- web/app/view3/view3.js
+++ web/app/view3/view3.js
@@ -1,21 +1,43 @@
 'use strict';
 
-angular.module('myApp.view3', ['ngRoute'])
+var app = angular.module('myApp.view3', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view3', {
-    templateUrl: 'app/view3/view3.html',
-    controller: 'View3Ctrl'
+    templateUrl: 'app/view3/view3.html'
   });
-}])
-
-.controller('View3Ctrl', function($http,$scope) {
-  $http.get('api/demoadmin')
-            .success(function (data, status, headers, config) {
-              $scope.data = data;
-            })
-            .error(function (data, status, headers, config) {
-              
-             });
- 
+}]);
+        app.controller('apictrl', function($scope, $http) {
+       
+        $http({
+        method : "GET",
+        skipAuthorization: true,
+        url : "http://cvrapi.dk/api?vat=3167%208021&country=dk",
+//         headers: {
+//   
+//   'User-Agent' : "CVR API-CA3 SCHOOL Exercise-SEBASTIAN-cph-sr148@cphbusiness.dk"
+//   
+// }
+    }).then(function(response) {
+   
+        var list = [];
+        $scope.members = response.data;
+        $scope.productionunits = response.data.productionunits;
+        $scope.heads = Object.keys($scope.productionunits);
+        $scope.llama = $scope.productionunits.entries();
+        
+        console.log($scope.llama[1]);
+        
+        $scope.productionunits2 = function(list)
+        {
+         list = response.data.productionunits;
+         return list;   
+        };
+        
+        /*
+        for(var i = 0; i <$scope.members.length; i++)
+        {
+        $scope.members2 = $scope.members[i];
+        } */
+    }); 
 });
--- web/app/view5/view5.js
+++ web/app/view5/view5.js
@@ -1,9 +1,34 @@
 'use strict';
 
-angular.module('myApp.view5', ['ngRoute'])
+var app = angular.module('myApp.view5', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view5', {
     templateUrl: 'app/view5/view5.html'
   });
 }]);
+        app.controller('apictrl', function($scope, $http) {
+       
+        $http({
+        method : "GET",
+        skipAuthorization: true,
+        url : "http://cvrapi.dk/api?vat=3167%208021&country=dk",
+         headers: {
+   
+   'User-Agent' : "CVR API-CA3 SCHOOL Exercise-SEBASTIAN-cph-sr148@cphbusiness.dk"
+   
+ }
+    }).then(function(response) {
+   
+    
+        $scope.members = response.data;
+        $scope.productionunits = response.data.productionunits;
+        $scope.heads = Object.keys($scope.members);
+        
+        /*
+        for(var i = 0; i <$scope.members.length; i++)
+        {
+        $scope.members2 = $scope.members[i];
+        } */
+    }); 
+});
