6687771213a867bf37888c678b3e2a8a734d4b6b
kaspe <kaspe@82.211.198.31>
2016-04-14 22:46:47 +0200

    med rest

--- web/app/view3/view3.js
+++ web/app/view3/view3.js
@@ -5,6 +5,7 @@ var app = angular.module('myApp.view3', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view3', {
     templateUrl: 'app/view3/view3.html'
+    
   });
 }]);
         app.controller('apictrl', function($scope, $http) {
--- web/app/view4/view4.js
+++ web/app/view4/view4.js
@@ -1,9 +1,30 @@
 'use strict';
 
-angular.module('myApp.view4', ['ngRoute'])
+var app = angular.module('myApp.view4', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view4', {
-    templateUrl: 'app/view4/view4.html'
-  });
-}]);
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view4', {
+                    templateUrl: 'app/view4/view4.html',
+                    controller: 'view4Control'
+                });
+            }]);
+
+app.controller('view4Control', ['$scope', "$http", function ($scope, $http) {
+        $scope.linket = "/SemesterSeed/api/currency/dailyrates";
+        console.log("asf");
+        $scope.rates = [];
+        $http(
+                {
+                    method: "GET",
+                    skipAuthorization: true,
+                    url: $scope.linket,
+                }
+        ).then(function (response)
+        {
+            
+            $scope.rates = response.data.rates;
+            console.log($scope.rates[1].currency);
+            $scope.refcur = response.data.refcur;
+            $scope.dato = response.data.dato;
+        });
+    }]);
